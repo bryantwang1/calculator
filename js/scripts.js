5572f1ca2a4070cf5cd13c413b51bd8301619ac9
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-19 11:37:49 -0700

    Add form input for subtract, multiply, divide.

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,43 @@ var divide = function(number1, number2) {
 
 
 $(function() {
+  var result;
+  var number1;
+  var number2;
+
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    number1 = parseInt($("#add1").val());
+    number2 = parseInt($("#add2").val());
 
-    var result = add(number1, number2);
+    result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    number1 = parseInt($("#subtract1").val());
+    number2 = parseInt($("#subtract2").val());
+
+    result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    number1 = parseInt($("#multiply1").val());
+    number2 = parseInt($("#multiply2").val());
+
+    result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    number1 = parseInt($("#divide1").val());
+    number2 = parseInt($("#divide2").val());
+
+    result = divide(number1, number2);
     $("#output").text(result);
   });
 });
