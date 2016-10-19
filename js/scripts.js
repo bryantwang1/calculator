07be932e851ce8d068b2169cd8a9875932bbe863
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-19 11:22:15 -0700

    Convert calculator to use form instead of prompts and alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,13 @@ var divide = function(number1, number2) {
 };
 
 
-
 $(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  
-  var result = divide(number1, number2);
-  alert(result);
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
